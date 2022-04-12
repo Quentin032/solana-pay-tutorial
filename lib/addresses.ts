a61fa05b489e45e369be2ee30545b2dc8a2f400e
Quentin032 <hfjty245353@hotmail.com>
2022-04-12 21:59:11 +0900

    second

---
+++ lib/addresses.ts
@@ -0,0 +1,6 @@
+import { PublicKey } from "@solana/web3.js"
+
+// Your shop wallet address
+export const shopAddress = new PublicKey('CdAFYoNCmPwN2Yhcoe8PNzqgUojuA6NJqAUaNCVJ3fqd') 
+// this is the same for everyone!
+export const usdcAddress = new PublicKey('Gh9ZwEmdLJ8DscKNTkTqPbNwLNNBjuSzaG9Vp2KGtKJr')
